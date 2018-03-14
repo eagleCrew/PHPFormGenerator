a8d8aed1cd1d0b8133802db0c539579cdbff9711
Grayson King <grayson.king065@gmail.com>
2018-03-14 05:14:53 +0000

    Adding images and index files

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Eagle Homes Document Creator</title>
+    <img src="images/eh_old_logo.PNG" alt="Eagle Homes Old Logo"> 
+        
+        <h1>This is the new Eagle Homes Document Creation Form</h1>
+        <h4>Currently Under Construction</h4>
+        
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
